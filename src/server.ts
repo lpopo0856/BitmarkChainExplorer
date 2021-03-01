c255b4fdcc12f9f8167522641ee4f6d384d4f95e
brdnyeh <jiji0856@gmail.com>
2021-03-02 03:21:27 +0800

    fix: small bug

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app"
-import config from "config/ormConfig"
+import config from "./config/ormConfig"
 
 (new app(config)).app.listen(3000, () => {
     console.log('Express server listening on Port ', 3000);
