3a07e109903f4fc3e2dbb91d8e93dce025a54705
brdnyeh <jiji0856@gmail.com>
2021-03-04 12:30:42 +0800

    feat: add websocket service

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
 import app from "./app"
+import { Server, Socket } from "socket.io";
 import config from "./config/ormConfig"
+import { latestBlock } from  "./utils/websocket";
 
 (new app(config)).app.listen(3000, () => {
     console.log('Express server listening on Port ', 3000);
 })
+
+export const io = new Server(3030, {
+  cors: {
+    origin: "http://localhost:8080",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connect", (socket: Socket) => {
+    socket.emit("latestBlock", latestBlock);
+});
+
+import * as schedule from "node-schedule"
+import { webSocketUtil } from "./utils/websocket"
+
+const job = schedule.scheduleJob('*/3 * * * * *', function () {
+    new webSocketUtil().getLatestBlock()
+});
---
+++ src/utils/websocket.ts
@@ -0,0 +1,29 @@
+import { Block } from "entities"
+import { Socket } from "socket.io"
+import { io } from "../server"
+import blockService from "../services/blockService"
+
+// store latest block
+export let latestBlock: Block[] = []
+
+// store sent latest block
+let sentBlock: Block[] = []
+
+export class webSocketUtil {
+    private blockService: blockService
+
+    constructor() {
+        this.blockService = new blockService()
+    }
+
+    public async getLatestBlock() {
+        latestBlock = await this.blockService.getBlocks(1)
+        JSON.stringify(sentBlock) != JSON.stringify(latestBlock) ? this.sendLatestBlock() : null
+    }
+
+    private sendLatestBlock() {
+        io.emit('latestBlock', latestBlock)
+        // deep copy
+        sentBlock = JSON.parse(JSON.stringify(latestBlock))
+    }
+}
